448cc446b0fb7f14ba923abead49bb6c5abc073e
Timofey <102862163+Shamyk2009@users.noreply.github.com>
2023-01-06 09:45:09 +0300

    Скрипт для загрузки сцен

---
+++ SceneLoader.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using SystemCollections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoader : MonoBehavior
+{
+	[SerializeField] private GameObject loadingScreen;
+	[SerializeField] private Slider loadingBar;
+	
+	public void loadScene(string sceneName) 
+	{
+		
+	}
+	
+	IEnumerator LoadSceneAsync(string name) 
+	{
+		AsyncOperation operation = SceneManager.LoadSceneAsync(name);
+		
+		loadingScreen.SetActive(true);
+		
+		while (!operation.isDone) 
+		{
+			loadingBar.value = operation.progress;
+			
+			yield return null;
+		}
+	}
+}
